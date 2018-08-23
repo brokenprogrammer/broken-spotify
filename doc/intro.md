8b68c56a5b0b44f6e026342a2f21b22e4aa74fdb
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-23 19:30:03 +0200

    Initial lein library generated.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to broken-spotify
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/broken_spotify/core_test.clj
@@ -0,0 +1,7 @@
+(ns broken-spotify.core-test
+  (:require [clojure.test :refer :all]
+            [broken-spotify.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
