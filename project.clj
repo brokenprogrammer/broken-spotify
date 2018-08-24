c6ae547fdf4a5389fb08967ed15d23bb0413a67b
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-24 04:55:20 +0200

    Added required HTTP client dependency.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.9.1"]])
--- src/broken_spotify/core.clj
+++ src/broken_spotify/core.clj
@@ -1,4 +1,7 @@
-(ns broken-spotify.core)
+(ns broken-spotify.core
+  (:require [clj-http.client :as client]))
+
+(def spotify-api-url "https://api.spotify.com/v1/")
 
 (defn foo
   "I don't do a whole lot."
