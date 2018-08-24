3e15ec1e22095ca137dce5d1a72428594c9f8c56
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-24 08:10:20 +0200

    Implemented get-access-token and refresh-access-token.

--- src/broken_spotify/core.clj
+++ src/broken_spotify/core.clj
@@ -2,6 +2,24 @@
   (:require [clj-http.client :as client]))
 
 (def spotify-api-url "https://api.spotify.com/v1/")
+(def spotify-api-token-url "https://accounts.spotify.com/api/token")
+
+(defn get-access-token
+  "TODO: Documentation"
+  [client_id client_secret]
+  (:access_token (:body 
+    (client/post spotify-api-token-url {:form-params {:grant_type "client_credentials"}
+                                        :basic-auth [client_id client_secret]
+                                        :as :json}))))
+
+(defn refresh-access-token
+  "TODO: Documentation"
+  [client_id client_secret refresh_token]
+  (:access_token (:body
+    (client/post spotify-api-token-url {:form-params {:grant_type "refresh_token"
+                                                      :refresh_token refresh_token}
+                                        :basic-auth [client_id client_secret]
+                                        :as :json}))))
 
 (defn foo
   "I don't do a whole lot."
