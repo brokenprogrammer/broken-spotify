7979ef38484c271f61a0fed1a4296cb15a8ede86
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-24 04:41:11 +0200

    Updated project.clj, License and Readme.md

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject broken-spotify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A library for the Spotify Web API."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
