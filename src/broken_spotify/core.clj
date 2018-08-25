04ede6094c0c3a074c5d0caced4ce2a8c74ac50a
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-25 06:18:40 +0200

    Added helper function to split url path parameters.

--- src/broken_spotify/core.clj
+++ src/broken_spotify/core.clj
@@ -1,8 +1,12 @@
 (ns broken-spotify.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.string :as string]))
 
 (def spotify-api-url "https://api.spotify.com/v1/")
 (def spotify-api-token-url "https://accounts.spotify.com/api/token")
+
+(def spotify-path-params [:id :category_id :owner_id :playlist_id :user_id])
+(def spotify-query-params [:market :limit :offset :ids])
 
 (defn get-access-token
   "Requests an access token from Spotify by following the Client Credentials Flow.
@@ -23,6 +27,21 @@
                                                       :refresh_token refresh_token}
                                         :basic-auth [client_id client_secret]
                                         :as :json}))))
+
+;TODO: Test this properly.. 
+(defn replace-path-params
+  "TODO: Add documentation."
+  [params url]
+  (if (string/blank? url)
+    ""
+    (let [found-keys (select-keys params spotify-path-params)
+          split-url (string/split (string/replace url "https://" "") #"/")]
+      (str "https://"
+        (string/join "/" 
+          (map 
+            (fn [x] (if (contains? found-keys (keyword x))
+              (get found-keys (keyword x))
+              x))split-url))))))
 
 (defn get-an-album 
   ""
